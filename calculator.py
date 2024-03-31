3e910fadf0f5a3c873b1281403f11329d9fc453d
Imamuddin Shaik <120667356+Shaik-Imamuudin@users.noreply.github.com>
2024-03-31 20:40:19 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,31 @@
+while True:
+    print("Select operation\n1.Add\n2.Subtract\n3.Multiply\n4.Divide")
+    choice = input("Enter choice(1/2/3/4): ")
+    if choice not in ('1', '2', '3', '4'):
+        print("Invalid input!")
+    else:
+        if choice == '1':
+            num1 = int(input("Enter first number: "))
+            num2 = int(input("Enter second number: "))
+            print(num1, "+", num2, "=", num1+num2)
+
+        elif choice == '2':
+            num1 = int(input("Enter first number: "))
+            num2 = int(input("Enter second number: "))
+            print(num1, "-", num2, "=", num1-num2)
+
+        elif choice == '3':
+             num1 = int(input("Enter first number: "))
+             num2 = int(input("Enter second number: "))
+             print(num1, "*", num2, "=", num1*num2)
+
+        elif choice == '4':
+            num1 = int(input("Enter first number: "))
+            num2 = int(input("Enter second number: "))
+            print(num1, "/", num2, "=", num1/num2)
+    next_calculation = input("Let's do next calculation? (yes/no): ").lower()
+    if next_calculation == "no":
+        break
+    else:
+        print("Enter yes or no")
+print("Thank You for using my calculator")
